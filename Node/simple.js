88dfdd7f4e7c0268c8d9853f796282787b92d870
Abhay Maurya <127673236+AbhayGRT@users.noreply.github.com>
2024-08-21 11:30:59 +0530

    Update simple.js

--- Node/simple.js
+++ Node/simple.js
@@ -1,11 +1,20 @@
-const calc = require('./calculator')
-// const {add,PI} = require(./calculator)
-console.log("Welcome" );
-console.log(calc.message)
+// const calc = require('./calculator')
+// const {add,PI} = require('./calculator')
+// const calc = require('./calcESModule.mjs')
 
-let addResult = calc.add(5,6);
+console.log("Welcome" );
+// console.log(calc.message)
+
+let addResult = add(5,6);
 
 console.log("Add result" + addResult)
-let area = calc.PI * 3 * 3;
+let area = PI * 3 * 3;
 
 console.log("Area" + area)
+
+// To import a ES6 module dynamically
+import ('./calcESModule.mjs').then(
+    es => {console.log(es.default);
+        console.log(es.multiply(10,10))
+    }
+)
